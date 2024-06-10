e5046661f7aaf2a148088e16270c1e92a18fd780
형언 <ilsin302a@gmail.com>
2024-06-10 20:34:27 +0900

    ✨ Add sign up dialog

--- app/src/main/java/kr/pandadong2024/babya/signup/SignupFragment.kt
+++ app/src/main/java/kr/pandadong2024/babya/signup/SignupFragment.kt
@@ -1,5 +1,7 @@
 package kr.pandadong2024.babya.signup;
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
@@ -8,6 +10,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.lifecycleScope
@@ -40,6 +45,23 @@ class SignupFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentSignupBinding.inflate(inflater, container, false)
+
+
+        binding.birthDayBtn.setOnClickListener {
+            dateService(binding.birthDayEdit)
+        }
+
+        binding.weddingDayBtn.setOnClickListener {
+            dateService(binding.weddingDayEdit)
+        }
+
+        binding.pregnancyDayBtn.setOnClickListener {
+            dateService(binding.pregnancyDayEdit)
+        }
+
+        binding.cityBtn.setOnClickListener {
+            locationSave(binding.cityEdit)
+        }
 
         // emailDelete 버튼 누르면 텍스트 삭제
         binding.emailDeleteBtn.setOnClickListener {
@@ -97,6 +119,18 @@ class SignupFragment : Fragment() {
                     binding.reemailBtn.isEnabled = false
                 }
             }
+            binding.reemailEdit?.doAfterTextChanged { text ->
+                Log.d(TAG, "onCreateView: 12345")
+                val reemailText = text.toString()
+                if (reemailText.length == 6){
+                    Log.d(TAG, "onCreateView: 성공!!")
+                    binding.checkEmail.isEnabled = true
+                } else{
+                    binding.checkEmail.isEnabled = false
+                    Log.d(TAG, "onCreateView: 실패!!")
+                }
+            }
+
             binding.passWordEdit?.doAfterTextChanged { text ->
                 val passwordText = text.toString()
                 if (passwordText.isNotEmpty() && passwordText.matches(Pattern.passwordRegex)) {
@@ -111,6 +145,109 @@ class SignupFragment : Fragment() {
         }
         return binding.root
     }
+
+    // 날짜 지정
+    private fun dateService(edit : EditText) {
+        val dialog = AlertDialog.Builder(context).create()
+        val edialog : LayoutInflater = LayoutInflater.from(context)
+        val mView : View = edialog.inflate(R.layout.calendar_dialog_view, null)
+
+        val year : NumberPicker = mView.findViewById(R.id.yearDatePicker)
+        val month : NumberPicker = mView.findViewById(R.id.monthDatePicker)
+        val day : NumberPicker = mView.findViewById(R.id.dayDatePicker)
+        val cancel : Button = mView.findViewById(R.id.cancelBtn)
+        val save : Button = mView.findViewById(R.id.saveBtn)
+
+
+        year.wrapSelectorWheel = false
+        month.wrapSelectorWheel = false
+        day.wrapSelectorWheel = false
+
+        year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+        month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+        day.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+
+
+        month.maxValue = 12
+        month.minValue = 1
+        day.maxValue = 31
+        day.minValue = 1
+
+        // 연도를 역순으로 표시할 배열 생성
+        val years = (2024 downTo 1900).map { it.toString() }.toTypedArray()
+
+        // 최소값과 최대값 설정
+        year.minValue = 0
+        year.maxValue = years.size - 1
+
+        // displayedValues 설정
+        year.displayedValues = years
+
+        // 기본값 설정 (2024)
+        year.value = 0
+
+
+        cancel.setOnClickListener {
+            dialog.dismiss()
+            dialog.cancel()
+        }
+
+        save.setOnClickListener {
+            val selectedYear = years[year.value]
+            val selectedDate = "$selectedYear-${String.format("%02d", month.value)}-${String.format("%02d", day.value)}"
+            edit.setText(selectedDate)
+            dialog.dismiss()
+            dialog.cancel()
+        }
+
+
+        dialog.setView(mView)
+        dialog.create()
+        dialog.show()
+    }
+
+    private fun locationSave(edit : EditText){
+        val dialog = AlertDialog.Builder(context).create()
+        val edialog : LayoutInflater = LayoutInflater.from(context)
+        val mView : View = edialog.inflate(R.layout.zone_dialog_view, null)
+
+        val city : NumberPicker = mView.findViewById(R.id.cityPicker)
+        val cancel : Button = mView.findViewById(R.id.cancelBtn)
+        val save : Button = mView.findViewById(R.id.saveBtn)
+
+        val cities = arrayOf("서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "세종특별시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도")
+        city.minValue = 0
+        city.maxValue = cities.size - 1
+        city.displayedValues = cities
+
+        cancel.setOnClickListener {
+            dialog.dismiss()
+            dialog.cancel()
+        }
+
+        save.setOnClickListener {
+            val selectedCity = cities[city.value]
+            edit.setText(selectedCity)
+            dialog.dismiss()
+            dialog.cancel()
+        }
+
+        dialog.setView(mView)
+        dialog.create()
+        dialog.show()
+    }
+
+
+//    private fun callBack(dtae : String, mode : Int){
+//        if (mode == 1){
+//            binding.birthDayEdit.setText(dtae)
+//        } else if (mode == 2){
+//            binding.weddingDayEdit.setText(dtae)
+//        } else if (mode == 3){
+//            binding.pregnancyDayEdit.setText(dtae)
+//        }
+//    }
+
 
     private fun addChildren() {
         val childrenName = binding.childrenEdit.text.toString()
@@ -154,6 +291,7 @@ class SignupFragment : Fragment() {
         }
     }
 
+    // 이메일 인증 보내기
     private fun reemail() {
         val email = binding.emailEdit.text.toString()
         binding.reemailLayout.visibility = View.VISIBLE
@@ -170,6 +308,7 @@ class SignupFragment : Fragment() {
         }
     }
 
+    // 이메일 인증 확인
     private fun verifyEmail() {
         binding.reemailBtn.isEnabled = false
         val email = binding.emailEdit.text.toString()
@@ -189,6 +328,7 @@ class SignupFragment : Fragment() {
         }
     }
 
+    // 이메일 인증 성공 시
     private fun handleSuccess() {
         lifecycleScope.launch(Dispatchers.Main) {
             Toast.makeText(requireContext(), "이메일이 인증되었습니다", Toast.LENGTH_SHORT).show()
@@ -199,6 +339,7 @@ class SignupFragment : Fragment() {
         }
     }
 
+    // 이메일 인증 실패 시
     private fun handleFailure(throwable: Throwable) {
         throwable.printStackTrace()
         lifecycleScope.launch(Dispatchers.Main) {
@@ -209,6 +350,7 @@ class SignupFragment : Fragment() {
     }
 
 
+    // 태아여부
     private fun pregnancyCheck(checkedId: Int) {
         when (checkedId) {
             R.id.pregnancyRadioYesButton -> {
@@ -221,6 +363,7 @@ class SignupFragment : Fragment() {
     }
 
 
+    // 아이여부
     private fun childrenCheck(checkedId: Int) {
         when (checkedId) {
             R.id.childrenRadioYesButton -> binding.childrenLayout.visibility = View.VISIBLE
@@ -228,6 +371,7 @@ class SignupFragment : Fragment() {
         }
     }
 
+    // 약관동의
     private fun fullAgreementCheck() {
         val isChecked = binding.fullAgreementCheckBox.isChecked
         binding.agreementCheckBox1.isChecked = isChecked
@@ -237,6 +381,32 @@ class SignupFragment : Fragment() {
     }
 
 
+    // 지역코드로 변환
+    private fun cityNumber(city: String): String {
+        return when (city) {
+            "서울특별시" -> "11"
+            "부산광역시" -> "21"
+            "대구광역시" -> "22"
+            "인천광역시" -> "23"
+            "광주광역시" -> "24"
+            "대전광역시" -> "25"
+            "울산광역시" -> "26"
+            "세종특별자치시" -> "29"
+            "경기도" -> "31"
+            "강원도" -> "32"
+            "충청북도" -> "33"
+            "충청남도" -> "34"
+            "전라북도" -> "35"
+            "전라남도" -> "36"
+            "경상북도" -> "37"
+            "경상남도" -> "38"
+            "제주특별자치도" -> "39"
+            else -> "00"
+        }
+    }
+
+
+    // 회원가입
     private fun Signup(){
         // 필수 항목
         val email = binding.emailEdit.text.toString()
@@ -244,16 +414,19 @@ class SignupFragment : Fragment() {
         val name = binding.nameEdit.text.toString()
         val birthDay = binding.birthDayEdit.text.toString()
         val city = binding.cityEdit.text.toString()
+        val locationCode = cityNumber(city)
         val checkBox1 = binding.agreementCheckBox1.isChecked
         val checkBox2 = binding.agreementCheckBox2.isChecked
         val checkBox3 = binding.agreementCheckBox3.isChecked
+
 
         // 선택 항목
         val marriedDay :String = binding.weddingDayEdit.text.toString()
 
 
 
-        if (name.isNotEmpty() && birthDay.isNotEmpty() && city.isNotEmpty() && passwordCheck == true && checkBox1 == true && checkBox2 == true && checkBox3 == true && emailCheck == true){
+
+        if (name.isNotEmpty() && birthDay.isNotEmpty() && locationCode.isNotEmpty() && passwordCheck == true && checkBox1 == true && checkBox2 == true && checkBox3 == true && emailCheck == true){
             lifecycleScope.launch(Dispatchers.IO){
                 kotlin.runCatching {
                     RetrofitBuilder.getSignupService().postSignup(
@@ -264,7 +437,7 @@ class SignupFragment : Fragment() {
                             marriedDt = marriedDay,
                             pregnancyDt = marriedDay,
                             birthDt = birthDay,
-                            locationCode = city,
+                            locationCode = locationCode,
                             pushToken = "", // fcm
                             childList = birthNameList + childrenNameList
                         )
