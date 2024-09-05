1a56ed95184568afbf116209aaa5bebfbae82527
hom237 <jinsic4568@gmail.com>
2024-09-05 20:56:48 +0900

    :fire: - clear code

--- app/src/main/java/kr/pandadong2024/babya/home/policy/PolicyContentFragment.kt
+++ app/src/main/java/kr/pandadong2024/babya/home/policy/PolicyContentFragment.kt
@@ -5,17 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import kr.pandadong2024.babya.R
 import kr.pandadong2024.babya.databinding.FragmentPolicyContentBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PolicyContentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PolicyContentFragment : Fragment() {
     var _binding: FragmentPolicyContentBinding? = null
-    private val binding = _binding!!
+    private val binding get()= _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +19,9 @@ class PolicyContentFragment : Fragment() {
     ): View? {
         _binding = FragmentPolicyContentBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_policy_content, container, false)
+        binding.signUpBackButton.setOnClickListener {
+            findNavController().navigate(R.id.action_policyContentFragment_to_policyMainFragment)
+        }
+        return binding.root
     }
 }
