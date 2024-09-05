f6a5a618c5e8c71a6c722a31306bcb491c9ad4a4
hom237 <jinsic4568@gmail.com>
2024-09-05 17:01:38 +0900

    :sparkles: make view adapter and join fragment

--- app/src/main/java/kr/pandadong2024/babya/home/policy/PolicyMainFragment.kt
+++ app/src/main/java/kr/pandadong2024/babya/home/policy/PolicyMainFragment.kt
@@ -1,16 +1,22 @@
 package kr.pandadong2024.babya.home.policy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kr.pandadong2024.babya.databinding.FragmentPolicyMainBinding
+import kr.pandadong2024.babya.home.todo_list.adapter.PolicyCategoryAdapter
+import kr.pandadong2024.babya.home.todo_list.decoration.PolicyCategoryItemDecoration
 
 class PolicyMainFragment : Fragment() {
+    val TAG = "PolicyMainFragment"
 
-    var _binding : FragmentPolicyMainBinding? = null
+    var _binding: FragmentPolicyMainBinding? = null
     val binding get() = _binding!!
+
+    private var _category : MutableList<String> = mutableListOf<String>("지역")
 
 
     override fun onCreateView(
@@ -18,6 +24,40 @@ class PolicyMainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPolicyMainBinding.inflate(inflater, container, false)
+
+
+        setCategory(categoryList = _category)
+
         return binding.root
+    }
+
+    private fun setCategory(
+        categoryList: List<String>,
+    ) {
+        val todoCategoryAdapter = PolicyCategoryAdapter(
+            localCategoryList = categoryList,
+            flash = { position, localCategoryList ->
+
+                if (position != 0){
+                    localCategoryList.removeAt(position)
+                    _category = localCategoryList
+                }
+                else{
+                    Log.d(TAG, "show aaa")
+                    // TODO : show BottomSheet
+                }
+
+                setCategory(localCategoryList)
+            }
+        )
+        todoCategoryAdapter.notifyItemRemoved(0)
+        with(binding) {
+            categoryRecyclerView.adapter = todoCategoryAdapter
+            Log.d("TAG", "itemDecorationCount : ${categoryRecyclerView.itemDecorationCount}")
+            if (categoryRecyclerView.itemDecorationCount == 0) categoryRecyclerView.addItemDecoration(
+                PolicyCategoryItemDecoration(10, 0, categoryList.size)
+            )
+
+        }
     }
 }
--- app/src/main/java/kr/pandadong2024/babya/home/policy/adapter/PolicyCategoryAdapter.kt
+++ app/src/main/java/kr/pandadong2024/babya/home/policy/adapter/PolicyCategoryAdapter.kt
@@ -2,43 +2,57 @@ package kr.pandadong2024.babya.home.todo_list.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Visibility
 import kr.pandadong2024.babya.R
+import kr.pandadong2024.babya.databinding.ItemPolicyListTagBinding
 import kr.pandadong2024.babya.databinding.ItemTodoListTagBinding
 
-class PolicyCategoryAdapter(val flash :(position:Int)->Unit, val categoryList : List<String>, val selectedItemPosition : Int) : RecyclerView.Adapter<PolicyCategoryAdapter.CategoryViewHolder>() {
-    inner class CategoryViewHolder(private val binding : ItemTodoListTagBinding) :  RecyclerView.ViewHolder(binding.root){
-        fun bind(themeText : String, position: Int){
-            binding.root.setOnClickListener {
-//                flash(position)
-            }
-            binding.itemTodoParent.setOnClickListener {
-                flash(position)
-            }
-//            binding.itemTodoParent
-            if(position != selectedItemPosition){
-                Log.d("isSelect", "test : $themeText")
-                binding.itemTodoParent.setBackgroundResource(R.drawable.sp_item_category_unselect_background)
-            }
-            else{
-                binding.itemTodoParent.setBackgroundResource(R.drawable.sp_item_category_select_background)
+class PolicyCategoryAdapter(val flash :(position:Int, localCategoryList : MutableList<String>)->Unit, private val localCategoryList : List<String>) : RecyclerView.Adapter<PolicyCategoryAdapter.CategoryViewHolder>() {
+    inner class CategoryViewHolder(private val binding: ItemPolicyListTagBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(localText: String, position: Int) {
+            if (position == 0) {
+                binding.localTagText.text = localText
+                binding.itemPolicyParent.visibility = View.GONE
+                binding.itemLocalParent.setOnClickListener {
+                    // TODO : show bottom Sheet
+                    flash(position, localCategoryList.toMutableList())
+                }
+            } else {
+                binding.policyTagText.text = localText
+                binding.itemLocalParent.visibility = View.GONE
+                binding.itemPolicyParent.setOnClickListener {
+                    //TODO : delete category
+                    flash(position, localCategoryList.toMutableList())
+                }
+                Log.d("isSelect", "test : $localText")
             }
 
         }
 
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
-        val binding = ItemTodoListTagBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        Log.d("test","test3")
+        val binding =
+            ItemPolicyListTagBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CategoryViewHolder(binding)
     }
 
-    override fun getItemCount(): Int  = categoryList.size
-
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        holder.bind(
-            themeText =  categoryList[position],
-            position = position)
+            Log.d("test","test")
+            holder.bind(localCategoryList[position], position)
     }
+
+    override fun getItemCount(): Int {
+        Log.d("test","test1 : ${localCategoryList}")
+      return  localCategoryList.size
+    }
+
+
+
 }
