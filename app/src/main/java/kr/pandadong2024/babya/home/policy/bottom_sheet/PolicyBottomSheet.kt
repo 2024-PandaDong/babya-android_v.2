618475b78a37c083f8059712a85b3926a168da57
hom237 <jinsic4568@gmail.com>
2024-09-05 21:35:36 +0900

    :sparkles: make & init BottomSheet

---
+++ app/src/main/java/kr/pandadong2024/babya/home/policy/bottom_sheet/PolicyBottomSheet.kt
@@ -0,0 +1,11 @@
+package kr.pandadong2024.babya.home.policy.bottom_sheet
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class PolicyBottomSheet(
+    tagList: MutableList<String>,
+    submit: (List<String>) -> Unit
+) : BottomSheetDialogFragment() {
+
+
+}
