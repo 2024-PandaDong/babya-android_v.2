9bc3187958a179b25ee97de6a7b2e182a27c3a07
hyeong-eon <ilsin302a@gmail.com>
2024-09-11 17:25:10 +0900

    ✨ Add singup view

--- app/src/main/java/kr/pandadong2024/babya/start/signup/Signup4.kt
+++ app/src/main/java/kr/pandadong2024/babya/start/signup/Signup4.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import kr.pandadong2024.babya.R
 
-class Signup2 : Fragment() {
+
+class Signup4 : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup2, container, false)
+        return inflater.inflate(R.layout.fragment_signup4, container, false)
     }
-
 }
