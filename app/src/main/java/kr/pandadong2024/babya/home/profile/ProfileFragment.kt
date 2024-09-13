dc3a1fda327e157ceb585742c11d66911cb89d46
hom237 <jinsic4568@gmail.com>
2024-09-13 15:50:43 +0900

    :sparkles: show version

--- app/src/main/java/kr/pandadong2024/babya/home/profile/ProfileFragment.kt
+++ app/src/main/java/kr/pandadong2024/babya/home/profile/ProfileFragment.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import coil.load
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -61,6 +62,16 @@ class ProfileFragment : Fragment() {
         val toolbar: androidx.appcompat.widget.Toolbar = view.findViewById(R.id.profileToolbar)
         // 툴바에 메뉴를 인플레이트
         toolbar.inflateMenu(R.menu.profile_menu)
+        val packageInfo = context?.packageManager?.getPackageInfo(requireContext().packageName, 0)
+        val versionName = packageInfo?.versionName // 버전 이름 (예: "1.0")
+
+        binding.appVersionText.text = "v$versionName"
+
+
+
+        binding.profileModifyView.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_profileModifyFragment)
+        }
 
 
         toolbar.setOnMenuItemClickListener{item ->
@@ -140,104 +151,11 @@ class ProfileFragment : Fragment() {
         // 정보 받기
         lifecycleScope.launch {
             getProfileData()
-//            profileDiary()
-//            profileBoard()
         }
 
-//        binding.profileBackButton.setOnClickListener {
-//            findNavController().navigate(R.id.action_profileFragment_to_mainFragment)
-//        }
-
-//        binding.boardMoveBtn.setOnClickListener {
-////            findNavController().navigate(R.id.action_profileFragment_to_dashBoardFragment)
-//        }
-//
-//        binding.diaryMoveBtn.setOnClickListener {
-//            findNavController().navigate(R.id.action_profileFragment_to_diaryFragment)
-//        }
-
-//        // 즐겨찾기 화면으로 이동
-//        binding.bookmarkBtn.setOnClickListener {
-//            goBookmark()
-//        }
 
         return binding.root
     }
-
-//    private fun dashBoardRecyclerView() {
-//        Log.d(TAG, "dashBoardRecyclerView: $boardList")
-//        boardAdapter = ProfileBoardAdapter(boardList!!) { id ->
-//            lifecycleScope.launch(Dispatchers.Main) {
-//                Log.d(TAG, "dashBoardRecyclerView: $id")
-//                kotlin.runCatching {
-//                    dashBoardViewModel.id.value = id
-////                    findNavController().navigate(R.id.action_profileFragment_to_detailDashBoardFragment)
-//                }
-//            }
-//        }
-//        boardAdapter.notifyDataSetChanged()
-////        binding.boardRv.adapter = boardAdapter
-//    }
-//
-//    private fun diaryRecyclerView() {
-//        Log.d(TAG, "diaryRecyclerView: $diaryList")
-//        diaryAdapter = ProfileDiaryAdapter(diaryList!!) { id ->
-//            lifecycleScope.launch(Dispatchers.Main) {
-//                Log.d(TAG, "diaryRecyclerView: ${id}")
-//                kotlin.runCatching {
-//                    diaryViewModel.id.value = id
-//                    findNavController().navigate(R.id.action_profileFragment_to_detailWriterFragment)
-//                }
-//            }
-//        }
-////        diaryAdapter.notifyDataSetChanged()
-////        binding.diaryRv.adapter = diaryAdapter
-//    }
-
-    // 게시판 정보 받기
-//    private fun profileBoard() {
-//        Log.d(TAG, "profileBoard: 들어옴")
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            kotlin.runCatching {
-//                val dashBoardData: BaseResponse<List<ProfileMyDashBoardResponses>> =
-//                    RetrofitBuilder.getProfileService().getMyDashBoard(
-//                        accessToken = "Bearer $token",
-//                        page = 1,
-//                        size = 100
-//                    )
-//                boardList = dashBoardData.data
-//            }.onSuccess {
-//                Log.d(TAG, "boardList: $boardList")
-//                launch(Dispatchers.Main) {
-//                    dashBoardRecyclerView()
-//                }
-//            }.onFailure { result ->
-//                result.printStackTrace()
-//            }
-//        }
-//    }
-//
-//    // 산모일기 정보 받기
-//    private fun profileDiary() {
-//        lifecycleScope.launch(Dispatchers.IO){
-//            kotlin.runCatching {
-//                val diaryData: BaseResponse<List<ProfileMyDiaryResponses>> =
-//                    RetrofitBuilder.getProfileService().getMyDiary(
-//                        accessToken = "Bearer $token",
-//                        page = 1,
-//                        size = 100
-//                    )
-//                diaryList = diaryData.data
-//            }.onSuccess {
-//                Log.d(TAG, "diaryList: ${diaryList}")
-//                launch(Dispatchers.Main){
-//                    diaryRecyclerView()
-//                }
-//            }.onFailure {
-//                it.printStackTrace()
-//            }
-//        }
-//    }
 
     // 프로필 정보 받기
     private fun getProfileData() {
@@ -255,8 +173,6 @@ class ProfileFragment : Fragment() {
 
                 launch(Dispatchers.Main) {
                     binding.welcomeText.text = "${result.data?.nickname}님 반가워요!"
-//                    binding.argText.text = "나이: ${result.data?.age}살"
-//                    binding.dayText.text = "D-Day: ${result.data?.dDay}일"
 
                     if (result.data?.profileImg == null) {
                         binding.profileImage.load(R.drawable.ic_basic_profile)
@@ -264,11 +180,6 @@ class ProfileFragment : Fragment() {
                         binding.profileImage.load(result.data.profileImg)
                     }
 
-//                    binding.weddingYearText.text = if (result.data?.marriedYears == 0) {
-//                        "결혼: 미혼"
-//                    } else {
-//                        "결혼: ${result.data?.marriedYears}년차"
-//                    }
                 }
             }.onFailure { result ->
                 Log.d(TAG, "onCreateView: ${result.message}")
@@ -278,10 +189,6 @@ class ProfileFragment : Fragment() {
         }
     }
 
-    // 즐겨찾기 화면으로 이동
-//    private fun goBookmark() {
-//        findNavController().navigate(R.id.action_profileFragment_to_bookmarkFragment)
-//    }
 
 
 
