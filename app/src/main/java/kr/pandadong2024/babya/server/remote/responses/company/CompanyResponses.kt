805c200adec4a50c7dc7e8138e9f7092f375712f
hyeong-eon <ilsin302a@gmail.com>
2024-10-13 21:02:16 +0900

    ✨ 회사 찾기 뷰 혜택 추가

--- app/src/main/java/kr/pandadong2024/babya/server/remote/responses/company/CompanyResponses.kt
+++ app/src/main/java/kr/pandadong2024/babya/server/remote/responses/company/CompanyResponses.kt
@@ -1,5 +1,7 @@
 package kr.pandadong2024.babya.server.remote.responses.company
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class CompanyResponses(
@@ -9,6 +11,7 @@ data class CompanyResponses(
     val link: String? ="",
     @field:SerializedName("description") // 설명
     val description: String? = "",
+    // 헤택 및 복지
     @field:SerializedName("mtrLvPeriod") // 육아 휴직 기간
     val mtrLvPeriod: Int? = -1,
     @field:SerializedName("mtrLvSalary") // 육아 휴직 급여 비율
@@ -29,6 +32,8 @@ data class CompanyResponses(
     val subsdType: String? = "",
     @field:SerializedName("subsdMoney") // 보조금 지원 액
     val subsdMoney: Int? = -1,
+
+    // 회사 설명
     @field:SerializedName("minSalary") // 최소 연봉
     val minSalary: Int? = -1,
     @field:SerializedName("maxSalary") // 최대 연봉
@@ -55,4 +60,77 @@ data class CompanyResponses(
     val companyType: String? = "",
     @field:SerializedName("contentImg") // 이미지
     val contentImg: String? = ""
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(link)
+        parcel.writeString(description)
+        parcel.writeValue(mtrLvPeriod)
+        parcel.writeValue(mtrLvSalary)
+        parcel.writeString(mtrLvIsSalary)
+        parcel.writeValue(mtrSupMoney)
+        parcel.writeString(mtrSupCondition)
+        parcel.writeValue(telComDays)
+        parcel.writeString(telComIsCan)
+        parcel.writeValue(telComTime)
+        parcel.writeString(subsdType)
+        parcel.writeValue(subsdMoney)
+        parcel.writeValue(minSalary)
+        parcel.writeValue(maxSalary)
+        parcel.writeValue(avgSalary)
+        parcel.writeValue(salaryYear)
+        parcel.writeValue(femalePeople)
+        parcel.writeValue(malePeople)
+        parcel.writeString(ceo)
+        parcel.writeString(tel)
+        parcel.writeString(address)
+        parcel.writeString(historyYear)
+        parcel.writeString(businessContent)
+        parcel.writeString(companyType)
+        parcel.writeString(contentImg)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<CompanyResponses> {
+        override fun createFromParcel(parcel: Parcel): CompanyResponses {
+            return CompanyResponses(parcel)
+        }
+
+        override fun newArray(size: Int): Array<CompanyResponses?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
